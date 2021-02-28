7f4cb70b4a660a743fc32cc4a5f6ee45b5870dd7
Natalya POida <nataly.poida@gmail.com>
2021-02-28 11:05:38 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 == 1) {
+        return false;
+    }
+    var result = new Array();
+    for (var i = 0; i <= str.length - 1; i++) {
+        var find = false;
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                find = true;
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                    if (result[result.length - 1] == bracketsConfig[j][0]) {
+                        result.pop();
+                        break;
+                    }
+                }
+                result.push(bracketsConfig[j][1]);
+                break;
+            }
+        }
+        if (!find) {
+            if (result.length == 0 || str[i] != result[result.length - 1]) {
+                return false;
+            }
+            result.pop();
+        }
+    }
+    return result.length == 0 ? true : false;
+};
